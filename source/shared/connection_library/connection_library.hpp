b3b24326eb4b4b29dcd98230f15ef320ae5ad454
Tim W <ti***er@live.de>
2016-06-12 15:58:47 +0200

    Added a shared headder for the connection library, which contains a message_headder struct.

---
+++ source/shared/connection_library/connection_library.hpp
@@ -0,0 +1,14 @@
+#ifndef CONNECTION_LIBRARY_HPP
+#define CONNECTION_LIBRARY_HPP
+
+typedef struct{
+
+    uint16_t type;
+    uint16_t lenght;
+    uint16_t sequence;
+
+} message_headder_t
+
+
+
+#endif
